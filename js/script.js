57e4b71928ff0264b23719a5604ba5124136150f
Danilo <danilo.colloca91@gmail.com>
2020-12-11 16:10:38 +0100

    punteggiatura nel feedback della console

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ var risultatoPsw;
 //definisco il contenuto della variabile "nome"
 
 nome = prompt ('Come ti chiami?');
-console.log('l\'utente si chiama ' + nome);
+console.log('l\'utente si chiama: ' + nome);
 
 //definisco il contenuto della variabile "cognome"
 
 cognome = prompt ('Ciao ' + nome + ' Qual\'è il tuo cognome?');
-console.log('il cognome dell\'utente è ' + cognome);
+console.log('il cognome dell\'utente è: ' + cognome);
 
 //definisco il contenuto della variabile "colorePrefe" (colore preferito)
 
@@ -24,7 +24,7 @@ console.log('il colore preferito di ' + nome + ' ' + cognome + ' è ' + colorePr
 //definisco il contenuto della variabile "anno" (darà l'anno corrente)
 
 anno = new Date().getFullYear();
-console.log('verifica anno corrente ' + anno);
+console.log('verifica anno corrente: ' + anno);
 
 /* definisco il contenuto della variabile "risultatoPsw" e lo "stampo" sul foglio HTML
 */
