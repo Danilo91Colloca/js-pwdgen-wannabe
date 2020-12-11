4093390aced5a8be66b84b49f98c6257eedaa1e7
Danilo <danilo.colloca91@gmail.com>
2020-12-11 16:08:53 +0100

    versione seria

--- js/simpatico-script.js
+++ js/simpatico-script.js
@@ -8,13 +8,13 @@ var risultatoPsw;
 
 //definisco il contenuto della variabile "nome"
 
-nome = prompt ('Come ti chiami?');
-console.log('l\'utente si chiama ' + nome);
+nome = prompt ('Rispondi alle mie domande per creare una password impenetrabile!!! Come ti chiami?');
+console.log('il tizio si chiama ' + nome);
 
 //definisco il contenuto della variabile "cognome"
 
 cognome = prompt ('Ciao ' + nome + ' Qual\'è il tuo cognome?');
-console.log('il cognome dell\'utente è ' + cognome);
+console.log('il cognome del tizio è ' + cognome);
 
 //definisco il contenuto della variabile "colorePrefe" (colore preferito)
 
@@ -24,11 +24,8 @@ console.log('il colore preferito di ' + nome + ' ' + cognome + ' è ' + colorePr
 //definisco il contenuto della variabile "anno" (darà l'anno corrente)
 
 anno = new Date().getFullYear();
-console.log('verifica anno corrente ' + anno);
 
 /* definisco il contenuto della variabile "risultatoPsw" e lo "stampo" sul foglio HTML
 */
 
-document.getElementById('result').innerHTML = 'la password tua password: ' + nome + cognome + colorePrefe + anno;
-
-console.log('risultato password visualizzata: ' + nome + cognome + colorePrefe + anno);
+document.getElementById('result').innerHTML = 'grazie alle informazioni che mi hai fornito ho creato la password più sicura del mondo per te : ' + nome + cognome + colorePrefe + anno + ' ...credici &#128586;';
